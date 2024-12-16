56bdc364265886ca5c4c7d3f71a52b4c246e6d54
Aurelios <jp.aureliostk@gmail.com>
2024-12-16 08:56:32 -0300

    Update header src -> root

--- root/js/header.js
+++ root/js/header.js
@@ -4,15 +4,15 @@ function loadHeader() {
         <button class="hamburger" aria-label="Toggle menu">☰</button>
 
         <figure class="logo">
-            <img src="../../../src/img/logo/logoAreaMCR.png" alt="areaMCRlogo"/>
+            <img src="../../../root/img/logo/logoAreaMCR.png" alt="areaMCRlogo"/>
         </figure>
         <nav class="navBox">
             <ul>
-                <li><a href="${location.pathname === '../../../../AreaMCR/src/html/inicio.html' ? '#' : '../../../../../AreaMCR/src/html/inicio.html'}">Página Inicial</a></li>
-                <!-- <li><a href="${location.pathname === '../../../../AreaMCR/src/html/contatos.html' ? '#' : '../../../AreaMCR/src/html/contatos.html'}">Contate-nos</a></li> --!>
-                <li><a href="${location.pathname === '../../../../AreaMCR/src/html/eventos.html' ? '#' : '../../../../../AreaMCR/src/html/eventos.html'}">Eventos</a></li>
-                <li><a href="${location.pathname === '../../../../AreaMCR/src/html/certificados.html' ? '#' : '../../../../../AreaMCR/src/html/certificados.html'}">Certificados</a></li>
-                <li><a href="${location.pathname === '../../../../AreaMCR/src/html/revista.html' ? '#' : '../../../../../AreaMCR/src/html/revista.html'}">Revista</a></li>
+                <li><a href="${location.pathname === '../../../../AreaMCR/root/html/inicio.html' ? '#' : '../../../../../AreaMCR/root/html/inicio.html'}">Página Inicial</a></li>
+                <!-- <li><a href="${location.pathname === '../../../../AreaMCR/root/html/contatos.html' ? '#' : '../../../AreaMCR/root/html/contatos.html'}">Contate-nos</a></li> --!>
+                <li><a href="${location.pathname === '../../../../AreaMCR/root/html/eventos.html' ? '#' : '../../../../../AreaMCR/root/html/eventos.html'}">Eventos</a></li>
+                <li><a href="${location.pathname === '../../../../AreaMCR/root/html/certificados.html' ? '#' : '../../../../../AreaMCR/root/html/certificados.html'}">Certificados</a></li>
+                <li><a href="${location.pathname === '../../../../AreaMCR/root/html/revista.html' ? '#' : '../../../../../AreaMCR/root/html/revista.html'}">Revista</a></li>
             </ul>
         </nav>
     </header>
